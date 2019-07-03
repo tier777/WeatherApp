9948fd15dde441234915f71c4421336cde3cb2fd
tier777 <tier1993@gmail.com>
2019-07-03 15:56:14 +0300

    Initial Commit

---
+++ WeatherTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherTest
+//
+//  Created by Nikita Gorobets on 03/07/2019.
+//  Copyright © 2019 ng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
